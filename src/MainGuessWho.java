39f6e46adf6c357795717fb47c6a7f11d945b30d
Kathryn <kathrynhofmann03@gmail.com>
2023-03-07 23:07:35 -0500

    ROUGH Beginning to the Guess Who GUI.

---
+++ src/MainGuessWho.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class MainGuessWho {
+    private JLabel GuessWhoLabel;
+    private JPanel FacesPanel;
+    private JPanel PersonPanel;
+    private JLabel QuestionBarLabel;
+    private JTextField QuestionBarTextField;
+    private JButton submitButton;
+}
