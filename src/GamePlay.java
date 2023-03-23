d364abdd96955fe24e72582161b7b7e477ea34ad
Zackary Pardue <me.z.par@gmail.com>
2023-03-23 18:34:13 -0400

    editing GamePLay

--- src/GamePlay.java
+++ src/GamePlay.java
@@ -4,15 +4,10 @@ import java.awt.event.MouseListener;
 import java.util.ArrayList;
 
 
-public class GamePlay {
-ArrayList<Portraits> player1GameBoard;
-ArrayList<Portraits> player2GameBoard;
-QuestionBar player1Question;
-QuestionBar player2Question;
-Portraits boardLoaction;
+public class GamePlay extends JPanel {
 
-int[] index = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23};
-    public void main(String[] args){
+
+
         /*
         1. Game starts, portraits selected by both players
         2. Player 1 submits a question to player 2
@@ -23,11 +18,12 @@ int[] index = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23};
          */
 
 
-        //Game Board creation
-        player1GameBoard = new ArrayList<>();
-        player2GameBoard = new ArrayList<>();
+        /*Game Board creation
+        1. "FacesPanel" generates a game board
+        2. Create two new panels that clone FacesPanel
+        3. When passing between players, the corresponding panel and Player portrait will flip
+         */
 
 
-    }
 
 }
